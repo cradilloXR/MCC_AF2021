5a2ddc9a7e13df690c8dc6c8f877bae3d137c3ef
nikola-kosaric <93000155+nikola-kosaric@users.noreply.github.com>
2021-11-01 17:27:32 -0500

    Updated simulated data
    Updated simulated data based on meeting today. Need to add button to show/him arc flash barrier & warning as well as bucket removal animation

--- src/phone/components/MCCAFDemo.js
+++ src/phone/components/MCCAFDemo.js
@@ -1,63 +1,38 @@
 var currentMin = 5;
-var CurrentMax = 7;
+var CurrentMax = 9;
 var voltageMin = 480;
 var voltageMax = 485;
-var powerMin = 1;
-var powerMax = 2;
 var frequencyMin = 70;
 var frequencyMax = 75;
 
 //1
 $scope.show1 = function() {
     $scope.view.wdg['imageButtonStatus']['imgsrc'] = 'app/resources/Uploaded/green_dot.svg';
-    $scope.view.wdg['label-1']['text']= 'MCC';
-  	$scope.view.wdg['label-2']['text']= '1';
+    $scope.view.wdg['label-1']['text']= 'MCC_1A';
+	$scope.view.wdg['label-2']['text']= 'Feeder Circuit Breaker';
   	$scope.view.wdg['labelFaultValue']['text']= 'No Fault';
 
     currentMin = 5;
-    currentMax = 7;
+    currentMax = 9;
     voltageMin = 480;
     voltageMax = 485;
-    powerMin = 1;
-    powerMax = 2;
-    frequencyMin = 70;
-	frequencyMax = 75;
-  
-  	$scope.app.fn.triggerWidgetService('unitInfoPopup','showpopup')
-}
-
-//2
-$scope.show2 = function() {
-    $scope.view.wdg['imageButtonStatus']['imgsrc'] = 'app/resources/Uploaded/red_dot.svg';
-  	$scope.view.wdg['label-1']['text']= 'MCC';
-  	$scope.view.wdg['label-2']['text']= '2';
-	$scope.view.wdg['labelFaultValue']['text']= 'F004 Under Voltage';
-  
-    currentMin = 5;
-    currentMax = 7;
-    voltageMin = 200;
-    voltageMax = 255;
-    powerMin = 1;
-    powerMax = 2;
-    frequencyMin = 70;
-    frequencyMax = 75;
+    frequencyMin = 60;
+	frequencyMax = 60;
   
   	$scope.app.fn.triggerWidgetService('unitInfoPopup','showpopup')
 }
 
 //3
 $scope.show3 = function() {
-    $scope.view.wdg['imageButtonStatus']['imgsrc'] = 'app/resources/Uploaded/brown_dot.svg';
-  	$scope.view.wdg['label-1']['text']= 'MCC';
-  	$scope.view.wdg['label-2']['text']= '3';
-	$scope.view.wdg['labelFaultValue']['text']= 'F008 Heatsink Over Temp';
+    $scope.view.wdg['imageButtonStatus']['imgsrc'] = 'app/resources/Uploaded/yellow_dot.svg';
+  	$scope.view.wdg['label-1']['text']= 'MCC_1E';
+  	$scope.view.wdg['label-2']['text']= 'PowerFlex 753';
+	$scope.view.wdg['labelFaultValue']['text']= 'Overtemp Condition Imminent';
   
     currentMin = 5;
-    currentMax = 7;
+    currentMax = 9;
     voltageMin = 480;
     voltageMax = 485;
-    powerMin = 1;
-    powerMax = 2;
     frequencyMin = 70;
     frequencyMax = 75;
   
@@ -66,17 +41,15 @@ $scope.show3 = function() {
 
 //4
 $scope.show4 = function() {
-    $scope.view.wdg['imageButtonStatus']['imgsrc'] = 'app/resources/Uploaded/yellow_dot.svg';
-  	$scope.view.wdg['label-1']['text']= 'MCC';
-  	$scope.view.wdg['label-2']['text']= '4';
-	$scope.view.wdg['labelFaultValue']['text']= 'Handswitch On';
+    $scope.view.wdg['imageButtonStatus']['imgsrc'] = 'app/resources/Uploaded/green_dot.svg';
+  	$scope.view.wdg['label-1']['text']= 'MCC_2A';
+  	$scope.view.wdg['label-2']['text']= 'PowerFlex 525';
+	$scope.view.wdg['labelFaultValue']['text']= 'No Fault';
   
     currentMin = 5;
-    currentMax = 7;
+    currentMax = 9;
     voltageMin = 480;
     voltageMax = 485;
-    powerMin = 1;
-    powerMax = 2;
     frequencyMin = 70;
     frequencyMax = 75;
   
@@ -85,31 +58,22 @@ $scope.show4 = function() {
 
 //5
 $scope.show5 = function() {
-    $scope.view.wdg['imageButtonStatus']['imgsrc'] = 'app/resources/Uploaded/white_dot.svg';
-  	$scope.view.wdg['label-1']['text']= 'MCC';
-  	$scope.view.wdg['label-2']['text']= '5';
-	$scope.view.wdg['labelFaultValue']['text']= 'No Fault';
+    $scope.view.wdg['imageButtonStatus']['imgsrc'] = 'app/resources/Uploaded/red_dot.svg';
+  	$scope.view.wdg['label-1']['text']= 'MCC_2E';
+  	$scope.view.wdg['label-2']['text']= 'PowerFlex 755';
+	$scope.view.wdg['labelFaultValue']['text']= 'F004 Under Voltage';
   
-    currentMin = 0;
-    currentMax = 0;
-    voltageMin = 0;
-    voltageMax = 0;
-    powerMin = 0;
-    powerMax = 0;
-    frequencyMin = 0;
-    frequencyMax = 0;
+    currentMin = 5;
+    currentMax = 7;
+    voltageMin = 200
+    voltageMax = 225
+    frequencyMin = 70;
+    frequencyMax = 75;
   
   	$scope.app.fn.triggerWidgetService('unitInfoPopup','showpopup')
 }
 
 //Flash icons that are faulted
-$interval(function() {
-	if ($scope.view.wdg['3DGauge-2'].opacity == 1) {
-        $scope.view.wdg['3DGauge-2'].opacity = 0.25;
-    } else {
-      	$scope.view.wdg['3DGauge-2'].opacity = 1;
-    }
-},500);
 $interval(function() {
 	if ($scope.view.wdg['3DGauge-3'].opacity == 1) {
         $scope.view.wdg['3DGauge-3'].opacity = 0.25;
@@ -118,10 +82,10 @@ $interval(function() {
     }
 },500);
 $interval(function() {
-	if ($scope.view.wdg['3DGauge-4'].opacity == 1) {
-        $scope.view.wdg['3DGauge-4'].opacity = 0.25;
+	if ($scope.view.wdg['3DGauge-5'].opacity == 1) {
+        $scope.view.wdg['3DGauge-5'].opacity = 0.25;
     } else {
-      	$scope.view.wdg['3DGauge-4'].opacity = 1;
+      	$scope.view.wdg['3DGauge-5'].opacity = 1;
     }
 },500);
 
@@ -133,7 +97,7 @@ $interval(function() {
 	$scope.view.wdg['gauge-2']['value'] = Math.floor(Math.random() * (voltageMax - voltageMin) ) + voltageMin;
 },1000);
 $interval(function() {
-  	$scope.view.wdg['gauge-3']['value'] = Math.floor(Math.random() * (powerMax - powerMin) ) + powerMin;
+  	$scope.view.wdg['gauge-3']['value'] = ($scope.view.wdg['gauge-1']['value'] * $scope.view.wdg['gauge-2']['value']) / 1000;
 },1000);
 $interval(function() {
   	$scope.view.wdg['gauge-4']['value'] = Math.floor(Math.random() * (frequencyMax - frequencyMin) ) + frequencyMin;
